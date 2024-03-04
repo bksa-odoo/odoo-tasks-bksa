4fbdaab2084d0e087628078d34e4d112eb8e3f2e
bksa-odoo <bksa@odoo.com>
2024-03-04 11:45:19 +0530

    Added new module

---
+++ stock_transport/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name" : "Dispatch Management System",
+    "version" : '1.0',
+    "author" : "Bibhav Shah",
+    "summary" : "A Module that implements TMS using batch picking",
+    "depends" : ['fleet','stock_picking_batch'],
+}
