e45e557f8d65b8c3e373f63132f61906590844a2
bksa-odoo <157004193+bksa-odoo@users.noreply.github.com>
2024-03-12 10:10:21 +0530

    Delete sales_commission_module directory

--- sales_commission_module/models/sale_order_lines.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api
-
-class SaleOrderLines(models.Model):
-    _inherit = 'sale.order.line'
-    
-    commission_lines = fields.Float('Commission')
-    
-    
-   
